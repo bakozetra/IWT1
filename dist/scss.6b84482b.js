2bdace2f5f2938610b2f79026ac94f418f5925c1
synthia <synthia.han@onja.org>
2021-01-18 15:41:19 +0300

    Fixing some css and added media query

--- dist/scss.6b84482b.js
+++ dist/scss.6b84482b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\icon-hamburger.svg":[["icon-hamburger.cb455b0f.svg","images/icon-hamburger.svg"],"images/icon-hamburger.svg"],"./..\\images\\bg-intro-mobile.svg":[["bg-intro-mobile.2783d013.svg","images/bg-intro-mobile.svg"],"images/bg-intro-mobile.svg"],"_css_loader":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\icon-hamburger.svg":[["icon-hamburger.cb455b0f.svg","images/icon-hamburger.svg"],"images/icon-hamburger.svg"],"./..\\images\\bg-intro-mobile.svg":[["bg-intro-mobile.2783d013.svg","images/bg-intro-mobile.svg"],"images/bg-intro-mobile.svg"],"./..\\images\\bg-intro-desktop.svg":[["bg-intro-desktop.d745898a.svg","images/bg-intro-desktop.svg"],"images/bg-intro-desktop.svg"],"_css_loader":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60688" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56443" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
